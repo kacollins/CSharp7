c74f3398a645a0bbbcae5aabe700ca5ec53ecf0d
kacollins <kimberlyacollins@gmail.com>
2016-07-10 18:16:48 -0500

    Added demo of binary literals.

--- CSharp7/Program.cs
+++ CSharp7/Program.cs
@@ -11,7 +11,18 @@ namespace CSharp7
     {
         static void Main(string[] args)
         {
+            DemoBinaryLiterals();
             Console.ReadKey();
-        }        
+        }
+
+        private static void DemoBinaryLiterals()
+        {
+            int five_Old = Convert.ToInt32("0101", 2);
+            int five_New = 0b0101;
+
+            Console.WriteLine("Binary Literals:");
+            Console.WriteLine($"{nameof(five_Old)} = {five_Old}");
+            Console.WriteLine($"{nameof(five_New)} = {five_New}");
+        }
     }
 }
