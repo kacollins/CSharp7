8ed099dd86f0484f864230f942c2fbc40f2771fe
kacollins <kimberlyacollins@gmail.com>
2016-07-10 18:14:03 -0500

    Created solution.

---
+++ CSharp7/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadKey();
+        }        
+    }
+}
